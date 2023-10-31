5cd5c2fc5fd5556ccf905a230d66b8cad1d39dfa
dev-msj <msj944@naver.com>
2023-11-01 08:09:47 +0900

    feat: futures entity 정의

---
+++ src/futures/entities/futures.entity.ts
@@ -0,0 +1,10 @@
+export class Futures {
+  futures_id: number;
+  futures_name: string;
+  futures_date: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+}

9130641c7e67790f4ad799d974400881d10b741d
dev-msj <msj944@naver.com>
2023-11-01 10:54:58 +0900

    refactor(futures.entity): 컬럼명 수정 및 데코레이터 추가
    Entity 데코레이터 추가
    컬럼명 변경
    PrimaryColumn, Column 데코레이터에 타입 정의

--- src/futures/entities/futures.entity.ts
+++ src/futures/entities/futures.entity.ts
@@ -1,10 +1,28 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity('Futrues')
 export class Futures {
+  @PrimaryColumn({ type: "int" })
   futures_id: number;
+
+  @PrimaryColumn({ type: "varchar", length: 50 })
   futures_name: string;
-  futures_date: string;
-  open: number;
-  high: number;
-  low: number;
-  close: number;
+
+  @Column({ type: "int" })
+  futures_date: Date;
+
+  @Column({ type: "int" })
+  open_price: number;
+
+  @Column({ type: "int" })
+  high_price: number;
+
+  @Column({ type: "int" })
+  low_price: number;
+
+  @Column({ type: "int" })
+  close_price: number;
+
+  @Column({ type: "int" })
   volume: number;
 }

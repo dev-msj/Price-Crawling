3e3b5a2215b04f9efd6c129bfbb37139e93c7851
dev-msj <msj944@naver.com>
2023-11-01 10:47:30 +0900

    style: 파일명 변경
    future -> futures

--- src/futures/dto/update-futures.dto.ts
+++ src/futures/dto/update-futures.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateFutureDto } from './create-future.dto';
+import { CreateFutureDto } from './create-futures.dto';
 
 export class UpdateFutureDto extends PartialType(CreateFutureDto) {}

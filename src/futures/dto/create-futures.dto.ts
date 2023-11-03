27a8a9eedc6c7ac74b864b9a6ed4aa8658f9f959
dev-msj <msj944@naver.com>
2023-11-04 05:40:49 +0900

    feat(create-futures.dto): CreateFufuresDto 정의

--- src/futures/dto/create-futures.dto.ts
+++ src/futures/dto/create-futures.dto.ts
@@ -1,1 +1,42 @@
-export class CreateFutureDto {}
+import { IsDate, IsNumber, IsString, MaxLength } from 'class-validator';
+import { Futures } from '../entities/futures.entity';
+
+export class CreateFuturesDto {
+  @IsNumber()
+  readonly futuresId: number;
+
+  @IsString()
+  @MaxLength(50)
+  readonly futuresName: string;
+
+  @IsDate()
+  readonly futuresDate: Date;
+
+  @IsNumber()
+  readonly openPrice: number;
+
+  @IsNumber()
+  readonly highPrice: number;
+
+  @IsNumber()
+  readonly lowPrice: number;
+
+  @IsNumber()
+  readonly closePrice: number;
+
+  @IsNumber()
+  readonly volume: number;
+
+  toFuturesEntity() {
+    return Futures.from(
+      this.futuresId,
+      this.futuresName,
+      this.futuresDate,
+      this.openPrice,
+      this.highPrice,
+      this.lowPrice,
+      this.closePrice,
+      this.volume,
+    );
+  }
+}
